ff246351cdd15271c6a305d86e0982b29e373efa
epiclad <epiclad@users.noreply.github.com>
2017-12-27 05:35:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-// header("Location: https://csgoreaper.com/"); die;
+header("Location: https://csgolotto.info/"); die;
 header("Content-Type: text/plain");
 echo 'contact: csgolotto@protonmail.ch';
